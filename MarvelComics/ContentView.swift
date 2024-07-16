f5a6e65895722575302298338593676cd8e372fe
Eric Hayes <egithub13@gmail.com>
2024-07-16 14:39:01 -0400

    CharacterDetailView added. CharacterRowView refactored out of main UI. Naviagation links added to navigation stack.

--- MarvelComics/ContentView.swift
+++ MarvelComics/ContentView.swift
@@ -13,28 +13,35 @@ struct ContentView: View {
   @State private var showAlert = false
   
   var body: some View {
-    NavigationStack {
-      List(viewModel.characters) { character in
-        NavigationLink(destination: CharacterDetailView()) {
-          Text(character.name)
+    CharacterRowView(viewModel: $viewModel)
+  }
+}
+
+struct CharacterRowView: View {
+  
+  @Binding var viewModel: ContentViewModel
+  var smallImg = "/standard_small."
+  var mediumImg = "/standard_medium."
+  
+  var body: some View {
+    VStack {
+      NavigationStack {
+        List(viewModel.characters) { character in
+          NavigationLink(destination: CharacterDetailView(character: character)) {
+            HStack {
+              AsyncImage(url: URL(string: character.thumbnail.path + smallImg + character.thumbnail.ext))
+              Text(character.name)
+            }
+          }
         }
+        .navigationTitle("Marvel Characters")
+        .navigationBarTitleDisplayMode(.inline)
       }
-    
-      .navigationTitle("Marvel Characters")
-      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
 
-struct BackgroundView: View {
-  var body: some View {
-    LinearGradient(gradient: Gradient(colors: [.red, .black]),
-                   startPoint: .topLeading,
-                   endPoint: .bottomTrailing)
-    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-    
-  }
-}
+
 #Preview {
   ContentView()
 }
--- MarvelComics/Views/CharacterDetailView.swift
+++ MarvelComics/Views/CharacterDetailView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct CharacterDetailView: View {
   
-  //var name: String
+  var character: Character
+  var largeImg = "/standard_fantastic."
   
-    var body: some View {
-        Text("3-D Man")
+  var body: some View {
+    ZStack {
+      BackgroundView()
+      VStack {
+        Spacer()
+        AsyncImage(url: URL(string: character.thumbnail.path + largeImg + character.thumbnail.ext))
+          .cornerRadius(12)
+        Text(character.name)
+          .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+          
+        Text(character.description)
+          .font(.body)
+          .padding(.horizontal)
+          .padding(.top)
+        Spacer()
+        Text("Data provided by Marvel. © 2014 Marvel")
+          .foregroundColor(.red)
+      }
+      .foregroundColor(.white)
     }
+  }
+}
+
+struct BackgroundView: View {
+  var body: some View {
+    LinearGradient(gradient: Gradient(colors: [.red, .black]),
+                   startPoint: .topLeading,
+                   endPoint: .bottomTrailing)
+    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    
+  }
 }
 
 #Preview {
-    CharacterDetailView()
+  CharacterDetailView(
+    character: Character(
+      id: 0001,
+      name: "3-D Man",
+      description: "He's a 3-d generated man that does stuff",
+      thumbnail: Thumbnail(
+        path: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784", ext: "jpg")))
 }
